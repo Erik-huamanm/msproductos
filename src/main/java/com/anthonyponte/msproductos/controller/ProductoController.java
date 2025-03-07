885f3ab4a4caf5b407ab18cde0bcb297adf53ded
Erik-huamanm <145626347+Erik-huamanm@users.noreply.github.com>
2025-03-07 11:04:49 -0500

    Update ProductoController.java
    Se agrega el Delete - Erik HM

--- src/main/java/com/anthonyponte/msproductos/controller/ProductoController.java
+++ src/main/java/com/anthonyponte/msproductos/controller/ProductoController.java
@@ -31,4 +31,13 @@ public class ProductoController {
                 .map(client -> ResponseEntity.ok(client))
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        if (clientService.findById(id).isPresent()){
+            clientService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
